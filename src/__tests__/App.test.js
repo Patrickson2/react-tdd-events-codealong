a2e1ea3225e5b261c2c149788db377b6665c0199
Patrickson2 <thairupatrickson@gmail.com>
2025-10-23 18:33:51 +0300

    made events

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,23 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event"; // Added import
 import App from "../App";
 
-import '@testing-library/jest-dom';
-
 // Code tests here
+test("selected topping disappears when checked a second time", () => {
+  render(<App />);
+
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
+  userEvent.click(addPepperoni);
+
+  expect(addPepperoni).toBeChecked();
+  expect(screen.getByText("Cheese")).toBeInTheDocument();
+  expect(screen.getByText("Pepperoni")).toBeInTheDocument();
+
+  userEvent.click(addPepperoni);
+
+  expect(addPepperoni).not.toBeChecked();
+  expect(screen.getByText("Cheese")).toBeInTheDocument();
+  expect(screen.queryByText("Pepperoni")).not.toBeInTheDocument();
+});
+
